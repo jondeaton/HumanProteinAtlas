20676aa3d4147e261d1a1d56e4c95617ea84bc70
Jon Deaton <jonpauldeaton@gmail.com>
2018-10-20 14:17:20 -0700

    Finished structure of data-loader

--- HumanProteinAtlas/data_set.py
+++ HumanProteinAtlas/data_set.py
@@ -10,29 +10,28 @@ from enum import Enum
 import csv
 from HumanProteinAtlas import sample
 
+
 class Split(Enum):
     test = 0
     train = 0
+
 
 class Dataset:
 
     def __init__(self, path, split=Split.train, cache=False):
         self.path = path
-        self.cache = False
+        self._cache = cache
 
         if split == Split.train:
             self.data_path = os.path.join(path, "train")
             self.csv_path = os.path.join(path, "train.csv")
+
         elif split == Split.test:
             self.data_path = os.path.join(path, "test")
             self.csv_path = os.path.join(path, "sample_submission.csv")
 
-        self._sample_labels = None
+        self._sample_labels = dict()
         self._samples = dict()
-
-    @property
-    def labels(self):
-        return
 
     @property
     def sample_ids(self):
@@ -40,20 +39,36 @@ class Dataset:
 
     @property
     def sample_labels(self):
-        if self._sample_labels is not None:
+        if self._sample_labels:  # not an empty dictionary
             return self._sample_labels
 
         self._sample_labels = self.read_labels(self.csv_path)
         return self._sample_labels
 
     def sample(self, sample_id):
-        if sample_id in
+        if sample_id in self._samples:
+            return self._samples[sample_id]
 
-        s = sample.Sample(sample_id)
-
+        s = sample.Sample(sample_id, self.path, cache=self._cache)
+        if self._cache:
+            self._samples[sample_id] = s
         return s
 
-    
+    def samples(self):
+        # caution: this function reads every single sample's
+        # meta-data into memory
+        for sample_id in self.sample_ids:
+            if sample_id not in self._samples:
+                self._samples[sample_id] = self.sample(sample_id)
+        return self._samples
+
+
+    def drop_cache(self):
+        for sample in self._samples.values():
+            sample.drop_cache()
+
+        self._samples = dict()
+        self._sample_labels = dict()
 
     @staticmethod
     def read_labels(csv_file):
--- HumanProteinAtlas/sample.py
+++ HumanProteinAtlas/sample.py
@@ -6,29 +6,158 @@ Author: Jon Deaton (jdeaton@stanford.edu)
 """
 
 import imageio
+import numpy as np
+from enum import Enum
+
+
+class Color(Enum):
+    red = 0
+    yellow = 1
+    green = 2
+    blue = 3
+
+
+colors = {
+    'red': Color.red,
+    'yellow': Color.yellow,
+    'green': Color.green,
+    'blue': Color.blue
+}
+
+color_names = {color: name for name, color in colors}
+
+default_color_ordering = {color: idx for idx, color in enumerate(colors.values())}
+
 
 class Sample:
-    def __init__(self, id, ):
-        self.id = id
-        self._red = None
-        self._green = None
-        self._blue = None
-        self._yellow = None
+    def __init__(self, sample_id, images_dir, cache=False,
+                 color_ordering=default_color_ordering):
+        # public
+        self.id = sample_id
+        self.color_ordering = color_ordering
 
-        @property
-        def red(self):
-            if self._red is not None:
-                return self._red
-            self.red = imageio.lo
+        # private
+        self._images_dir = images_dir
+        self._cache = cache
 
-        @property
-        def green(self):
-            pass
+        self._image_locations = dict()
+        self._images = dict()
+        self._combined = None
 
-        @property
-        def blue(self):
-            pass
+        self._shape = None
 
-        @property
-        def yellow(self):
-            pass
+    @property
+    def red(self):
+        return self.image(Color.red)
+
+    @property
+    def yellow(self):
+        return self.image(Color.yellow)
+
+    @property
+    def green(self):
+        return self.image(Color.green)
+
+    @property
+    def blue(self):
+        return self.image(Color.blue)
+
+    @property
+    def combined(self):
+        """
+
+        the first time that we call this all the images
+        which we may have loaded into the "_images" dictionary
+        get moved into the combined numpy array and are accessed
+        from that location in stead thereafter
+
+        :return:
+        """
+        if self._combined is not None:
+            return self._combined
+
+        combined = np.empty((4,) + self.shape)
+
+        for color in list(Color):
+
+            if color in self._images:
+                img = self._images[color]
+                del self._images[color]
+            else:
+                img = self._load_image(color)
+
+            idx = self.color_ordering[color]
+            combined[idx] = img
+
+        if self._cache:
+            self._combined = combined
+
+        return combined
+
+    def image(self, color):
+        assert isinstance(color, Color)
+
+        if self._combined is not None:
+            idx = self.color_ordering[color]
+            return self._combined[idx]
+
+        if color in self._images:
+            return self._images[color]
+
+        img = self._load_image(color)
+
+        if self._cache:
+            self._images[color] = img
+        return img
+
+    def drop_cache(self):
+        self._images = dict()
+        self._image_locations = dict()
+        self._combined = None
+
+    def show_single(self, color):
+        # debugging
+        if isinstance(color, str):
+            color = colors[color]
+        import matplotlib.pyplot as plt
+        plt.imshow(self.image(color))
+
+    def show(self):
+        # for debugging
+        import matplotlib.pyplot as plt
+        comb = np.empty((3,) + self.shape)
+        comb[0] = self.red
+        comb[1] = self.green
+        comb[2] = self.blue
+
+        comb[0] += self.yellow / 2
+        comb[1] += self.yellow / 2
+
+        plt.imshow(comb)
+
+    @property
+    def image_locations(self):
+        if self._image_locations:
+            return self._image_locations
+
+        self._image_locations = dict()
+        for color in list(Color):
+            self._image_locations[color] = self.location(color)
+        return self._image_locations
+
+    def location(self, color):
+        color_name = color_names[color]
+        return "{id}_{color}.png".format(id=self.id, color=color_name)
+
+    @property
+    def shape(self):
+        if self._shape is None:
+            self._shape = self.red.shape
+        return self._shape
+
+    def _load_image(self, color):
+        png_file = self.image_locations[color]
+        img = imageio.imread(png_file)
+        if self._shape is None:
+            self._shape = img.shape
+        return img
