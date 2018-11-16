db0e59779757ef93481466f4e884429f2418e027
Jon Deaton <jonpauldeaton@gmail.com>
2018-11-15 16:11:41 -0800

    Added evluation starter code

---
+++ deep_model/evluate.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+File: evluate
+Date: 11/15/18 
+Author: Jon Deaton (jdeaton@stanford.edu)
+"""
+
+import evaluation
+
+
+def main():
+    pass
+    # evaluation.evluate(model, data, labels)
+
+
+if __name__ == "__main__":
+    main()
