932e4231ff6c36fea95ee3c866baa7399f686fa6
nefrob <25070989+nefrob@users.noreply.github.com>
2018-12-07 22:19:09 -0800

    Finished off HoG

--- feature_extraction/feature_visualization.py
+++ feature_extraction/feature_visualization.py
@@ -6,6 +6,8 @@ Author: Robert Neff (rneff@stanford.edu)
 """
 
 import matplotlib.pyplot as plt
+from skimage import exposure
+from skimage.feature import hog
 import cv2
 
 
@@ -29,5 +31,27 @@ def show_lbp_features(lbp_image):
     ax.set_xlim([0, nbins])
     ax.set_ylim([0, 0.030])
     plt.show()
+
+
+"""
+Ref: http://scikit-image.org/docs/dev/auto_examples/features_detection/plot_hog.html
+"""
+def show_hog_features(image):
+	fd, hog_image = hog(image, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(2, 2), visualize=True, multichannel=True)
+
+	fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
+
+	ax1.axis('off')
+	ax1.imshow(image, cmap=plt.cm.gray)
+	ax1.set_title('Input image')
+
+	# Rescale histogram for better display
+	hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
+
+	ax2.axis('off')
+	ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
+	ax2.set_title('Histogram of Oriented Gradients')
+	plt.show()
+
 
 # TODO: add more feature visualization help, ex. plotting PCA for 2 features
