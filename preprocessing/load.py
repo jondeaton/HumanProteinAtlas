cb7b5eefe342bc223e1b23035d0d3f05a617a3c9
Jon Deaton <jonpauldeaton@gmail.com>
2018-12-10 18:30:52 -0800

    Trying to fix loader

--- preprocessing/load.py
+++ preprocessing/load.py
@@ -38,23 +38,23 @@ def load_dataset(dataset, split, classes=None):
     return tf.data.Dataset.from_generator(sample_generator, output_types=output_types, output_shapes=output_shapes)
 
 
-def load_gmm_dataset(human_protein_atlas, split, get_gmm_probabilities, gmm_num_latent_classes, classes=None):
+def load_gmm_dataset(human_protein_atlas, split, get_gmm_probabilities, gmm_num_latent_classes):
     assert isinstance(human_protein_atlas, Dataset)
     assert isinstance(split, Split)
-
-    if classes is not None:
-        classes = set(classes)
 
     sample_shape_shape = human_protein_atlas.shape[1:]
     label_shape = (len(Organelle),)
     probas_shape = (gmm_num_latent_classes,)
 
+    ids = partitions[id]
+
+    dataset = tf.data.Dataset.from_generator()
+
     def sample_generator():
         for sample_id in partitions[split]:
             sample = human_protein_atlas.sample(sample_id)
-            if classes is None or any(l in classes for l in sample.labels):
-                gmm_probas = get_gmm_probabilities(sample)
-                yield sample.multi_channel, gmm_probas, sample.multi_hot_label
+            gmm_probas = get_gmm_probabilities(sample)
+            yield sample.multi_channel, gmm_probas, sample.multi_hot_label
 
     output_types = (tf.float32, tf.float32, tf.float32)
     output_shapes = (tf.TensorShape(sample_shape_shape), tf.TensorShape(probas_shape), tf.TensorShape(label_shape))
